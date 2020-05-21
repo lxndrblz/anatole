292a8afde384c8ce887f41cc80b94d77b3514879
Alexander Bilz <mail@alexbilz.com>
2020-05-21 19:43:09 +0200

    Fixes issues with flickering in dark mode
    closes #14

---
+++ assets/js/anatole-header.js
@@ -0,0 +1,54 @@
+// initialize default value
+function getTheme(){
+    return localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+}
+
+function setTheme(style){
+    document.documentElement.setAttribute('data-theme', style);
+    localStorage.setItem('theme', style);  
+}
+
+function init(){
+    // initialize default value
+    var theme = getTheme();
+
+    // check if a prefered color theme is set for users that have never been to our site
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (theme === null) {
+        if(userPrefersDark){
+            setTheme('dark');
+        }
+        else{
+            setTheme('light');
+        }
+    }
+    else {
+        console.log(theme);
+        // load a stored theme
+        if (theme == 'light') {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }    
+    }    
+}
+
+
+// switch themes
+function switchTheme(e) {
+    var theme = getTheme();
+    if (theme == 'light') {
+        setTheme('dark');
+    }
+    else {
+        setTheme('light');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    themeSwitcher = document.querySelector('.theme-switch');
+    themeSwitcher.addEventListener('click', switchTheme, false);
+}, false);
+
+init();

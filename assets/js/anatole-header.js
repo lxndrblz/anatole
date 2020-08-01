c85e3b7b59288fd4961c0cd86d21db0d1ad318ad
Alexander Bilz <mail@alexbilz.com>
2020-08-01 13:10:09 +0200

    📱 Added proper mobile navbar
    Closes #36

--- assets/js/anatole-header.js
+++ assets/js/anatole-header.js
@@ -50,4 +50,21 @@ document.addEventListener('DOMContentLoaded', function() {
     themeSwitcher.addEventListener('click', switchTheme, false);
 }, false);
 
+document.addEventListener("DOMContentLoaded", function () {
+// Get all "navbar-burger" elements
+var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+// Check if there are any navbar burgers
+if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+    $el.addEventListener("click", function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+    });
+    });
+}
+});
+
 init();

64b32d0030e711d477835bdb58cc690dea62dd33
Carlo Corti <araknoid@gmail.com>
2021-01-01 18:06:17 +0100

    Now it's possible to disable the theme switcher

--- assets/js/anatole-header.js
+++ assets/js/anatole-header.js
@@ -1,53 +1,3 @@
-// initialize default value
-function getTheme() {
-    return localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-}
-
-function setTheme(style) {
-    document.documentElement.setAttribute('data-theme', style);
-    localStorage.setItem('theme', style);
-}
-
-function init() {
-    // initialize default value
-    var theme = getTheme();
-
-    // check if a prefered color theme is set for users that have never been to our site
-    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (theme === null) {
-        if (userPrefersDark) {
-            setTheme('dark');
-        } else if (!document.documentElement.getAttribute('data-theme')) {
-            setTheme('light');
-        } else {
-            setTheme(document.documentElement.getAttribute('data-theme'));
-        }
-    } else {
-        // load a stored theme
-        if (theme == 'light') {
-            document.documentElement.setAttribute('data-theme', 'light');
-        } else {
-            document.documentElement.setAttribute('data-theme', 'dark');
-        }
-    }
-}
-
-
-// switch themes
-function switchTheme(e) {
-    var theme = getTheme();
-    if (theme == 'light') {
-        setTheme('dark');
-    } else {
-        setTheme('light');
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    var themeSwitcher = document.querySelector('.theme-switch');
-    themeSwitcher.addEventListener('click', switchTheme, false);
-}, false);
-
 document.addEventListener("DOMContentLoaded", function () {
 // Get all "navbar-burger" elements
     var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
@@ -64,5 +14,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
-init();
---
+++ assets/js/anatole-theme-switcher.js
@@ -0,0 +1,51 @@
+// initialize default value
+function getTheme() {
+    return localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+}
+
+function setTheme(style) {
+    document.documentElement.setAttribute('data-theme', style);
+    localStorage.setItem('theme', style);
+}
+
+function init() {
+    // initialize default value
+    const theme = getTheme();
+
+    // check if a preferred color theme is set for users that have never been to our site
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    if (theme === null) {
+        if (userPrefersDark) {
+            setTheme('dark');
+        } else if (!document.documentElement.getAttribute('data-theme')) {
+            setTheme('light');
+        } else {
+            setTheme(document.documentElement.getAttribute('data-theme'));
+        }
+    } else {
+        // load a stored theme
+        if (theme === 'light') {
+            document.documentElement.setAttribute('data-theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+    }
+}
+
+
+// switch themes
+function switchTheme() {
+    const theme = getTheme();
+    if (theme === 'light') {
+        setTheme('dark');
+    } else {
+        setTheme('light');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeSwitcher = document.querySelector('.theme-switch');
+    themeSwitcher.addEventListener('click', switchTheme, false);
+}, false);
+
+init();

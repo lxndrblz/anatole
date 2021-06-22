4662ded338aa92fb7550274f373676e342e94260
alexanderdavide <alexeble1998@gmail.com>
2021-06-22 20:16:35 +0200

    feat: show footer on mobile; make aside fixed; rework css

--- assets/js/anatole-header.js
+++ assets/js/anatole-header.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 // Get all "navbar-burger" elements
     var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
+    var nav = document.querySelector("nav");
 // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
         // Add a click event on each of them
@@ -10,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 var $target = document.getElementById(target);
                 $el.classList.toggle("is-active");
                 $target.classList.toggle("is-active");
+                nav.classList.toggle("is-active");
             });
         });
     }
